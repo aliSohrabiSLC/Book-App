f4b754121696bea8d50bd8ec6dcdaedbb611c1ce
Ali Sohrabi <alisohrabislc@gmail.com>
2024-09-12 20:32:01 +0330

    first step make configuration of Book app project

---
+++ rjs-book-app-files-main/src/layouts/Layout.jsx
@@ -0,0 +1,16 @@
+
+import styles from "./Layout.module.css"
+function Layout({children}) {
+  return (
+    <>
+    <header className={styles.header}>
+        <h1>Book App</h1>
+        <p> <a href="https://github.com/aliSohrabiSLC">github </a> | React.js Full Course</p>
+    </header>
+    {children}
+    <footer className={styles.footer}><p>Developed by Ali Sohrabi With 😍</p></footer>
+    </>
+  )
+}
+
+export default Layout
